c6d23d84e8f4658a91e5e6e5e615d10d94d40665
Hamed Asemi <h-@live.se>
2016-12-26 20:42:01 +0100

    External modules

--- modules/@webrew/helpers-debug/index.js
+++
@@ -1,7 +0,0 @@
-export const DEBUG = typeof process.env.DEBUG !== `undefined`
-
-let module = {
-    DEBUG
-}
-
-export default module
--- modules/@webrew/helpers-env/env.js
+++
@@ -1,7 +0,0 @@
-export const ENV = typeof process.env.ENV !== `undefined` ? false : process.env.ENV
-
-let module = {
-    ENV
-}
-
-export default module
