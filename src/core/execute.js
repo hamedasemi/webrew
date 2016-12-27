3dd3a14d0027cd606fc01b6e52afe756a67ecbd4
Hamed Asemi <h-@live.se>
2016-12-27 21:00:44 +0100

    Format code

--- src/core/execute.js
+++ src/core/execute.js
@@ -3,11 +3,11 @@ import { debug, error } from 'webrew-helpers-log'
 import { spawn } from 'cross-spawn'
 
 export class Execute {
-    constructor(data){
+    constructor(data) {
         debug(`Constructed:`, data)
         try {
             require(`./../${data.category}/${data.command}`)
-            debug(`category:`, data.category, `command:`,  data.command, `options:`, data.options)
+            debug(`category:`, data.category, `command:`, data.command, `options:`, data.options)
 
             spawn(`gulp`, [
                 `${data.command}`,
@@ -19,7 +19,7 @@ export class Execute {
                 `babel-polyfill`
             ], { stdio: `inherit` })
         }
-        catch(err){
+        catch (err) {
             error(`Make sure command '${data.command}' and category '${data.category}' exist.`)
             error(`Original error:`, err)
         }
